5d30f17d4980aa6a2f1f2271add50ecc94413981
humaysafarova <166644215+humaysafarova@users.noreply.github.com>
2025-04-25 22:28:27 +0000

    commit

---
+++ news/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class NewsThree(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField()
+
+    def __str__(self):
+        return self.title 
---
+++ news/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from .models import NewsThree
+from .serializers import NewsThreeSerializer 
+
+class NewsThreeList(APIView):
+    def get(self, request):
+        queryset = NewsThree.objects.all()
+        serializer = NewsThreeSerializer(queryset, many=True)
+        return Response({'news': serializer.data})
